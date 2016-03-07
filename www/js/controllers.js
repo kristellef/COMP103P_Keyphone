d0e3255d102960f7f47fa8d660eca76ad42bcd71
kristelle feghali <zcabfeg@ucl.ac.uk>
2016-03-07 14:05:15 +0000

    Add keyPhone

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,23 +4,35 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('keyphoneMenuCtrl', function($scope) {
-
-})
-   
 .controller('settingsCtrl', function($scope) {
 
 })
    
-.controller('newListCtrl', function($scope) {
+.controller('everydayUseCtrl', function($scope) {
 
 })
    
-.controller('enterListNameCtrl', function($scope) {
+.controller('practiseCtrl', function($scope) {
 
 })
    
-.controller('editListCtrl', function($scope) {
+.controller('practise2Ctrl', function($scope) {
+
+})
+   
+.controller('practise3Ctrl', function($scope) {
+
+})
+   
+.controller('practise4Ctrl', function($scope) {
+
+})
+   
+.controller('practise5Ctrl', function($scope) {
+
+})
+   
+.controller('practise6Ctrl', function($scope) {
 
 })
  
--- www/js/routes.js
+++ www/js/routes.js
@@ -11,42 +11,60 @@ angular.module('app.routes', [])
   
 
       .state('keyphone', {
-    url: '/page1',
+    url: '/page6',
     templateUrl: 'templates/keyphone.html',
     controller: 'keyphoneCtrl'
   })
 
-  .state('keyphoneMenu', {
-    url: '/menu',
-    templateUrl: 'templates/keyphoneMenu.html',
-    controller: 'keyphoneMenuCtrl'
-  })
-
   .state('settings', {
-    url: '/page5',
+    url: '/page7',
     templateUrl: 'templates/settings.html',
     controller: 'settingsCtrl'
   })
 
-  .state('newList', {
-    url: '/page6',
-    templateUrl: 'templates/newList.html',
-    controller: 'newListCtrl'
-  })
-
-  .state('enterListName', {
+  .state('everydayUse', {
     url: '/page8',
-    templateUrl: 'templates/enterListName.html',
-    controller: 'enterListNameCtrl'
+    templateUrl: 'templates/everydayUse.html',
+    controller: 'everydayUseCtrl'
   })
 
-  .state('editList', {
-    url: '/page7',
-    templateUrl: 'templates/editList.html',
-    controller: 'editListCtrl'
+  .state('practise', {
+    url: '/page9',
+    templateUrl: 'templates/practise.html',
+    controller: 'practiseCtrl'
   })
 
-$urlRouterProvider.otherwise('/page1')
+  .state('practise2', {
+    url: '/page10',
+    templateUrl: 'templates/practise2.html',
+    controller: 'practise2Ctrl'
+  })
+
+  .state('practise3', {
+    url: '/page13',
+    templateUrl: 'templates/practise3.html',
+    controller: 'practise3Ctrl'
+  })
+
+  .state('practise4', {
+    url: '/page14',
+    templateUrl: 'templates/practise4.html',
+    controller: 'practise4Ctrl'
+  })
+
+  .state('practise5', {
+    url: '/page15',
+    templateUrl: 'templates/practise5.html',
+    controller: 'practise5Ctrl'
+  })
+
+  .state('practise6', {
+    url: '/page12',
+    templateUrl: 'templates/practise6.html',
+    controller: 'practise6Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/page6')
 
   
 
